6a6fd2986f76dbe52018d21a779521d9d4d4ea32
Neha <neha@Nehas-MacBook-Air.local>
2025-06-05 09:44:51 +0530

    creating section6

---
+++ assets/js/script.js
@@ -0,0 +1,81 @@
+$('.autoplay').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+                  
+
+
+
+
+// card slider
+
+$('.slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2.8,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    prevArrow: '.prev',
+    nextArrow: '.next',
+    // dots: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2.5,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 925,
+            settings: {
+                slidesToShow: 1.8,
+            }
+        },
+        {
+            breakpoint: 525,
+            settings: {
+                slidesToShow: 1,
+
+            }
+        }
+
+    ]
+});
+
+
+
+
+
